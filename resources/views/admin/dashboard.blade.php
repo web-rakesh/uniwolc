8a5fbb6170512cfab3e5aeac8dcc01590970b23e
web-rakesh <123937294+webmantratechdev@users.noreply.github.com>
2023-08-08 17:49:46 +0530

    dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,14 @@
 @extends('admin.layouts.layout')
 @section('content')
-   
+<div class="content-wrapper">
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
